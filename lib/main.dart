556085fe87179124a615953ed42e7752036cf997
Seungwoo hong <1100974+hongsw@users.noreply.github.com>
2023-04-19 01:13:23 +0900

    Add Three-line ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -21,21 +21,28 @@ class FirstScreen extends StatelessWidget {
             child: SingleChildScrollView(
           child: Column(
             children: [
-              Placeholder(
+              const Placeholder(
                 fallbackHeight: 200, // Placeholder의 높이를 지정합니다.
               ),
               ListView.builder(
                 shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: 15,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: 7,
                 itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    title: Text(' ListTile $index'),
+                  return Card(
+                    child: ListTile(
+                      leading: FlutterLogo(size: 72.0),
+                      title: Text('Three-line ListTile'),
+                      subtitle: Text(
+                          'A sufficiently long subtitle warrants three lines.  $index'),
+                      trailing: Icon(Icons.more_vert),
+                      isThreeLine: true,
+                    ),
                   );
                 },
               ),
-              Placeholder(
-                fallbackHeight: 200, // Placeholder의 높이를 지정합니다.
+              const Placeholder(
+                fallbackHeight: 100, // Placeholder의 높이를 지정합니다.
               ),
             ],
           ),
